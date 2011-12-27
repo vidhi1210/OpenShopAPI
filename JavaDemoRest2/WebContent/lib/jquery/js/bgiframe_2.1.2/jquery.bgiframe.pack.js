706398ecb21c3199633db00a5f56f126ecd34e0c
Administrator <Administrator@10.204.200.39>
2011-12-27 15:55:20 +0530

    New jquery libraries

---
+++ JavaDemoRest2/WebContent/lib/jquery/js/bgiframe_2.1.2/jquery.bgiframe.pack.js
@@ -0,0 +1,10 @@
+/* Copyright (c) 2006 Brandon Aaron (http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ * $LastChangedDate: 2011-03-24 17:38:00 -0100 (Th, 24 Mar 2011) $
+ * $Rev: 2446 $
+ *
+ * Version 2.1.2
+ */
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(4($){$.f.e=($.Z.p&&/p 6\\.0/i.N(E.D)?4(s){s=$.C({c:\'3\',b:\'3\',d:\'3\',9:\'3\',k:F,g:\'G:j;\'},s);J q=\'<l I="e"B="0"K="-1"g="\'+s.g+\'"\'+\'u="t:r;A:v;z-x:-1;\'+(s.k!==j?\'H:L(X=\\\'0\\\');\':\'\')+\'c:\'+(s.c==\'3\'?\'a(((o(2.8.m.W)||0)*-1)+\\\'7\\\')\':5(s.c))+\';\'+\'b:\'+(s.b==\'3\'?\'a(((o(2.8.m.11)||0)*-1)+\\\'7\\\')\':5(s.b))+\';\'+\'d:\'+(s.d==\'3\'?\'a(2.8.O+\\\'7\\\')\':5(s.d))+\';\'+\'9:\'+(s.9==\'3\'?\'a(2.8.Q+\\\'7\\\')\':5(s.9))+\';\'+\'"/>\';h 2.S(4(){R($(2).U(\'l.e\').T===0)2.P(12.w(q),2.y)})}:4(){h 2});$.f.10=$.f.e;4 5(n){h n&&n.M===V?n+\'7\':n}})(Y);',62,65,'||this|auto|function|prop||px|parentNode|height|expression|left|top|width|bgiframe|fn|src|return||false|opacity|iframe|currentStyle||parseInt|msie|html|block||display|style|absolute|createElement|index|firstChild||position|frameborder|extend|userAgent|navigator|true|javascript|filter|class|var|tabindex|Alpha|constructor|test|offsetWidth|insertBefore|offsetHeight|if|each|length|children|Number|borderTopWidth|Opacity|jQuery|browser|bgIframe|borderLeftWidth|document'.split('|'),0,{}))
