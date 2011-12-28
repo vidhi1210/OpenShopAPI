d0701572457fe766dd112dde0e24716b68199f5f
Administrator <Administrator@10.204.200.39>
2011-12-28 16:42:16 +0530

    Working mini cart

--- JavaDemoRest2/src/com/ecm/demo/rest/ViewCartServlet.java
+++ JavaDemoRest2/src/com/ecm/demo/rest/ViewCartServlet.java
@@ -8,56 +8,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-
 /**
  * Servlet implementation class ViewCart
  */
 public class ViewCartServlet extends HttpsServlet {
 	private static final long serialVersionUID = 1L;
        
-	public static final String DW_HOST="https://demo.ocapi.demandware.net/s/Demos-SiteGenesis-Site/dw/shop/v1/basket/this/!checkout";
-	public static final String CLIENT_ID = "client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	public static final String DW_URL="https://demo.ocapi.demandware.net/s/Demos-SiteGenesis-Site/dw/shop/v1/basket/this?client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&pretty_print=true";
+     
     public ViewCartServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
 		viewCart(request,response);
 	}
 
 	public void viewCart(HttpServletRequest request,
 			HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		String viewCartString = DW_HOST + "?" + CLIENT_ID;
-		
 		try {
-			webResource = getClient().resource(viewCartString); 
-			String s = webResource.post(String.class);
-			
-			response.setContentType("text/xml");
+			webResource = getClient().resource(DW_URL); 
+			String s = webResource.get(String.class);
 		    PrintWriter out = response.getWriter();
-			
 			out.println(s);
-			
 		}
 		catch (Exception e) {
 			e.printStackTrace();
